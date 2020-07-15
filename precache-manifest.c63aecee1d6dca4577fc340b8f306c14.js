adce907bfb382f9c4be6a11c13f99a985a28d583
David Acton <stanacton@gmail.com>
2020-07-15 13:26:02 +0100

    Publish checkin

---
+++ precache-manifest.c63aecee1d6dca4577fc340b8f306c14.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4149b91e96f6d69ef70f67a34f5cc579",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1aebd28c4173d16048e4",
+    "url": "./static/css/main.4c0793fb.chunk.css"
+  },
+  {
+    "revision": "51832639eb6cc8bbd8a1",
+    "url": "./static/js/2.b6a450eb.chunk.js"
+  },
+  {
+    "revision": "1481b120bc3fe08a69e94a89bc8bd66b",
+    "url": "./static/js/2.b6a450eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1aebd28c4173d16048e4",
+    "url": "./static/js/main.e4fb8179.chunk.js"
+  },
+  {
+    "revision": "edfe562ff85935909395",
+    "url": "./static/js/runtime-main.35415ced.js"
+  }
+]);
